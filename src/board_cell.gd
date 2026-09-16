bb21ce4baf9674bf40cd5361f73689f826f6b9a2
madowaku <67355875+madowaku@users.noreply.github.com>
2026-09-16 09:53:41 +0900

    feat: scaffold board cell visual

---
+++ src/board_cell.gd
@@ -0,0 +1,10 @@
+extends Button
+
+var candidate := false
+var player := false
+var preview := false
+var trail := false
+var revealed_watermelon := false
+var facing := Vector2i(0, -1)
+var stick_offset := Vector2i.ZERO
+var stick_visible := false
